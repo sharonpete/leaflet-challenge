58fc499f1c603c09ade14a15d5cb4aacf18581e2
Sharon Templin <sharon@hollygo.es>
2021-05-30 16:19:18 -0500

    Intial overlays and layers added.  Reading geojson file successfully.

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,13 +1,21 @@
 console.log('logic.js is loaded');
 
+// initialize the LayerGroups
+var layers = {
+    COMING_SOON: new L.LayerGroup()
+};
+
+// create the map with the layers
 var quakeMap = L.map("mapid", {
     //center: [45.52, -122.67],
     center: [48.3689, -103.77155634166667],
-    zoom: 4
+    zoom: 4, layers: [
+        layers.COMING_SOON
+    ]
 });
 
 // create the tile layer for the background of the map
-//var lightmap = 
+
 L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
     attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
     tileSize: 512,
@@ -19,55 +27,43 @@ L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?acce
 
 console.log('map loaded?');
 
-// initialize the LayerGroups
-// var layers = {
-//     COMING_SOON: new L.LayerGroup()
-// };
 
-// // create the map with the layers
-// var quakeMap = L.map("mapid", {
-//     center: [40.73, -74.0059],
-//     zoom: 12,
-//     layers: [
-//         layers.COMING_SOON
-//     ]
-// });
 
 // add 'lightmap' tile layer to the map
 //lightmap.addTo(quakeMap);
 
 // create overlays object to add to the layer control
-// var overlays = {
-//     'Coming Soon': layers.COMING_SOON
-// };
+var overlays = {
+    'Coming Soon': layers.COMING_SOON
+};
 
-// // control for layers, and add overlay layers to map
-// L.control.layers(null, overlays).addTo(quakeMap);
+// control for layers, and add overlay layers to map
+L.control.layers(null, overlays).addTo(quakeMap);
 
-// // legend to display information about quakeMap
-// var quakeInfo = L.control({
-//     position: 'bottomright'
-// });
+// legend to display information about quakeMap
+var quakeInfo = L.control({
+    position: 'bottomright'
+});
 
-// // insert div with class of 'legend'
-// quakeInfo.onAdd = function() {
-//     var div = L.DomUtil.create("div", "legend");
-//     return div;
-// };
+// insert div with class of 'legend'
+quakeInfo.onAdd = function() {
+    var div = L.DomUtil.create("div", "legend");
+    return div;
+};
 
-// // add the quakeInfo legend to the map
-// quakeInfo.addTo(quakeMap);
+// add the quakeInfo legend to the map
+quakeInfo.addTo(quakeMap);
 
-// // icons for each layer group
-// var quakeIcons = {
-//     COMING_SOON: L.ExtraMarkers.icon({
-//         icon: "ion-settings",
-//         iconColor: "red",
-//         markerColor: "yellow",
-//         shape: "triangle"
-//     })
-// };
+// icons for each layer group
+var quakeIcons = {
+    COMING_SOON: L.icon({
+        icon: "ion-settings",
+        iconColor: "red",
+        markerColor: "yellow",
+        shape: "triangle"
+    })
+};
 
-// d3.json("").then(function(data) {
-//     console.log(data);
-// });
+d3.json("../static/data/all_week.geojson").then(function(data) {
+    console.log(data);
+});
