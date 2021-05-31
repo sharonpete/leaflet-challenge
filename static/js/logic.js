eddd1579a4d74de6d630474fb26b7d1055119fcc
Sharon Templin <sharon@hollygo.es>
2021-05-30 22:49:38 -0500

    Map now working with the correct USGS URL for All Earthquakes for the past day.

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,12 +1,14 @@
 console.log('logic.js is loaded');
 
 // API endpoint
-var queryUrl = "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2014-01-01&endtime=" +
-  "2014-01-02&maxlongitude=-69.52148437&minlongitude=-123.83789062&maxlatitude=48.74894534&minlatitude=25.16517337";
+//var queryUrl = "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2014-01-01&endtime=" +
+  //"2014-01-02&maxlongitude=-69.52148437&minlongitude=-123.83789062&maxlatitude=48.74894534&minlatitude=25.16517337";
+
+var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson";
 
 // GET request to the query URL
-//d3.json(queryUrl).then(function(data) {
-d3.json("../static/data/all_week.geojson").then(function(data) {
+d3.json(queryUrl).then(function(data) {
+//d3.json("../static/data/all_week.geojson").then(function(data) {
     // get a response, sent the data.features object to the createFeatures function
     createFeatures(data.features);
 });
